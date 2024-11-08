aa6cefeb9bd5de5d9e8a0fdd9104b1e6be8a3289
Jorge Munárriz Gutiérrez-Solana <munarrizjorge@gmail.com>
2024-11-08 18:12:06 +0100

    screens creadas y barril actualizado

---
+++ lib/presentation/screens/animated/animated_screens.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AnimatedScreens extends StatelessWidget {
+  static const name = 'animated_screen';
+
+  const AnimatedScreens({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/presentation/screens/progress/progress_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class PorgressScreens extends StatelessWidget {
+  static const name = 'progress_screen';
+
+  const PorgressScreens({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
